ecfcb3ae7fc9008f9438aaf4ac6f81e01e479c20
Unknown <sdethy@gmail.com>
2017-11-14 11:27:43 +0100

    new files

--- Views/login.php
+++
@@ -1,7 +0,0 @@
-<form action="connexion.php" method="post">
-    Pseudo: <input type="text" name="pseudo" required value="" />
-
-    Mot de passe: <input type="password" name="mot_de_passe" required value="" />
-
-    <input type="submit" name="connexion" value="Connexion" />
-</form>
