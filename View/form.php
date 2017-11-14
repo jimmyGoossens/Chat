35a6520f15f188dc778facd04a18b694acec7410
Unknown <sdethy@gmail.com>
2017-11-14 09:57:54 +0100

    form.php corrections

--- View/form.php
+++ View/form.php
@@ -2,13 +2,13 @@
       <!-- contenair principale-->
   <div class="fullpage">
 
-      <form id="contact" name="contactForm" action="#" methode="post">
-        <fieldset><input type="text" size="14" id="name" placeholder="Pseudo"></fieldset>
-       <fieldset><input type="text" size="14" id="name" placeholder="Password"></fieldset>
-       <fieldset><input type="text" size="14" id="name" placeholder="confirmPassword"></fieldset>
-       <fieldset><input type="email" size="14" id="mail" placeholder="E-Mail"></fieldset>
-       <fieldset><input type="tel" size="14" id="phonel" placeholder="Confirm E-Mail"></fieldset>
-       <input type="submit" size="14" id="contactsubm" value="mychat">
+      <form id="contact" name="contactForm" action="inscription.php" methode="post">
+        <fieldset><input type="text" size="14" name="pseudo" id="pseudo" placeholder="Pseudo"></fieldset>
+       <fieldset><input type="text" size="14" name="mdp" id="mdp" placeholder="Password"></fieldset>
+       <fieldset><input type="text" size="14" name="mdp" id="ndp" placeholder="confirmPassword"></fieldset>
+       <fieldset><input type="email" size="14" name="mail" id="mail" placeholder="E-Mail"></fieldset>
+       <fieldset><input type="tel" size="14" name="mail" id="email" placeholder="Confirm E-Mail"></fieldset>
+       <input type="submit" size="14" id="contactsubm" value="send">
      </form>
 
     <!-- fin contenair principale-->
