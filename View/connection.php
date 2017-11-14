5b8e4357acc385b5576f0b21709d2581a0205947
ilias-chergui <chergui.ilias@gmail.com>
2017-11-14 10:07:17 +0100

    upload index.php

--- View/connection.php
+++ View/connection.php
@@ -3,7 +3,7 @@
 
 <?php
   // Lier le fichier à la base de donnée
-  $bdd = new PDO('mysql:host=10.20.0.158/phpmyadmin; bdname=test; charset=utf8', 'root', 'user');
+  $bdd = new PDO('mysql:host=10.20.0.158:8000; bdname=test; charset=utf8', 'root', 'user');
 
   // Ne  pas cacher le mot de passe
   $pass_hache = password_hash($_POST['pass']);
@@ -16,15 +16,18 @@
 
   $resultat = $req->fetch();
 
-  // Message si OK ou MAUVAIS
-  if (!$resultat) {
-    echo 'Mauvais identifiant ou mot de passe !';
-  } else {
-    session_start();
-    $_SESSION['id'] = $resultat['id'];
-    $_SESSION['pseudo'] = $pseudo;
-    echo 'Vous êtes connecté !';
+  // On sécurise la connection et on affiche un message si OK ou MAUVAIS
+  if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)#', $pass_hache)) {
+      if (!$resultat) {
+        echo 'Mauvais identifiant ou mot de passe !';
+      } else {
+          session_start();
+          $_SESSION['id'] = $resultat['id'];
+          $_SESSION['pseudo'] = $pseudo;
+          echo 'Vous êtes connecté !';
+        }
   }
+
 
 ?>
 
